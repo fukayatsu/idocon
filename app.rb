4116c4398918d16f935a29b923973cc802e46384
fukayatsu <fukayatsu@gmail.com>
2015-01-18 13:37:03 +0900

    Add sass

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'coffee-script'
 require 'sinatra'
+require 'sass'
 require 'slim'
 require 'idobata_hook'
 
@@ -22,4 +23,8 @@ end
 
 get '/app.js' do
   coffee :app
+end
+
+get '/app.css' do
+  sass :app
 end
